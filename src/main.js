1e2915cbf0f1c8fabe403753520e195d65d6bbe5
sundbyskii <riskbusiness@mail.ru>
2024-06-07 21:09:59 +0500

    Добавлено наследование, изменены классы, проведен рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-import { render } from './render.js';
-import TripPresenter from './presenter/presenter.js';
-import FilterView from './view/filter-view.js';
 import PointsModel from './model/points-model.js';
+import OffersModel from './model/offers-model.js';
+import DestinationsModel from './model/destinations-model.js';
+import PointsPresenter from './presenter/points-presenter.js';
+import FiltersPresenter from './presenter/filters-presenter.js';
+import TripInfoPresenter from './presenter/trip-info-presenter.js';
+import MockService from './service/mock-service.js';
 
-const siteMainElement = document.querySelector('.page-main');
-const siteHeaderElement = document.querySelector('.trip-main');
+const mockService = new MockService();
+const pointsModel = new PointsModel(mockService);
+const offersModel = new OffersModel(mockService);
+const destinationsModel = new DestinationsModel(mockService);
 
-const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'));
-const pointModel = new PointsModel();
+const pointsContainer = document.querySelector('.trip-events');
 
-render(new FilterView(), siteHeaderElement.querySelector('.trip-controls__filters'));
+const pointsPresenter = new PointsPresenter({
+  pointsContainer,
+  pointsModel,
+  offersModel,
+  destinationsModel
+});
 
-tripPresenter.init(siteMainElement.querySelector('.trip-events'), pointModel);
+const filtersPresenter = new FiltersPresenter();
+const tripInfoPresenter = new TripInfoPresenter();
+
+pointsPresenter.init();
+filtersPresenter.init();
+tripInfoPresenter.init();
