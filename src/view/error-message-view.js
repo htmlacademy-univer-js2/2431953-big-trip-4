5d7972ef2351e705d8adb7bcd92860eb51aa492f
sundbyskii <riskbusiness@mail.ru>
2024-06-09 04:50:04 +0500

    singlequotes

--- src/view/error-message-view.js
+++ src/view/error-message-view.js
@@ -1,7 +1,7 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const createErrorMessageTemplate = () => (
-  `<p class="trip-events__msg">Failed to load latest route information</p>`
+  '<p class="trip-events__msg">Failed to load latest route information</p>'
 );
 
 export default class ErrorMessageView extends AbstractView {
